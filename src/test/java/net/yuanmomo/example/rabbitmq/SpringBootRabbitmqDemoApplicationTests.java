e6ef02454be46688bcca8134371808f3258ca15f
袁 鸿彬 <hongbin.yuan@za.group>
2021-09-16 15:54:36 +0800

    Init commit

---
+++ src/test/java/net/yuanmomo/example/rabbitmq/SpringBootRabbitmqDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yuanmomo.example.rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRabbitmqDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
